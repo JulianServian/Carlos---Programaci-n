7ff7973a98c314be01b9db1a38e9ded94f863042
JulianServian <160215764+JulianServian@users.noreply.github.com>
2025-09-24 16:06:46 +0000

    Commit v1

--- Proyectos/proyecte-1-2/scripts/game.js
+++ Proyectos/proyecte-1-2/scripts/game.js
@@ -1,8 +1,7 @@
 console.log("Game script cargado");
 
-// Definir dimensiones del tablero
 let filas = 10;
 let columnas = 10;
 
-// Llamar a la función de renderizado
 renderizarTablero(filas, columnas);
+
--- Proyectos/proyecte-1-2/scripts/renderer.js
+++ Proyectos/proyecte-1-2/scripts/renderer.js
@@ -1,16 +1,19 @@
 console.log("renderer script cargado");
 
+// Función para renderizar el tablero
 function renderizarTablero(filas, columnas) {
     let contenedor = document.querySelector("#ContenedorDelJuego");
     contenedor.innerHTML = ""; 
 
 let añadirhtml = "";
 
+// Crear las celdas del tablero y añadirlas al contenedor
 for (let i = 0; i < filas; i++) {
   for (let j = 0; j < columnas; j++) {
     añadirhtml = añadirhtml + `<div class="celda"></div>`;
   }
 }
+
 
 contenedor.innerHTML = añadirhtml;
 }
